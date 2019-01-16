18a68535826203c797906e3bd85ae929a29cc53b
ycngu <snl123698745@163.com>
2019-01-16 10:00:31 +0800

    切换到发布环境

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -22,8 +22,9 @@ let url ={
 }
 
 //真实环境和开发环境切换
-let host = 'http://rap2api.taobao.org/app/mock/7058/'
+// let host = 'http://rap2api.taobao.org/app/mock/7058/'
 
+let host = 'https://www.easy-mock.com/mock/5c33439c0be8d31904699ad0/youzan' //发布环境
 for (let key in url) {
     if (url.hasOwnProperty(key)) {
         url[key] = host + url[key]
